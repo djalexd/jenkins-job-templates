a1060b87bea42522f8b363dd7f3d31f45ea7c351
Alexandru Dobjanschi <djalexd@users.noreply.github.com>
2015-12-02 22:48:30 +0200

    Multiple jobs, simpler creation

--- pipeline-factory.groovy
+++ pipeline-factory.groovy
@@ -1,45 +1,38 @@
 
-
- def guessProjectName(gitUrl) {
-  def index1 = gitUrl.indexOf("github.com")
-  if (index1 < 0) {
-    throw new IllegalArgumentException(gitUrl + " not recognized");
-  } else {
-    def subString1 = gitUrl.substring(index1 + "github.com".length() + 1);
-    subString1.substring(0, subString1.length() - 4);
-  }
-}
-
-def project = guessProjectName("${git_scm_url}")
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each {
-    def branchName = it.name
-    def jobName = "${job_base_name} - Unit Tests - ${branchName}".replaceAll('/','-')
-    job(jobName) {
-
-        label("swarm")
-
+    job("${project} - Unit Tests - ${it.name}".replaceAll('/','-')) {
+        /* the swarm label is a future extension */
+        /*label("swarm")*/
         scm {
-          
           git {
-            
             remote {
               url "${git_scm_url}"
             }
-
-            branch branchName
-            
+            branch it.name
             createTag false
             clean true
-            
           }
-        }
-        triggers {
-          scm('*/5 * * * *')
         }
         steps {
             maven("clean test")
+        }
+    }
+    
+    job("${project} - Integration Tests - ${it.name}".replaceAll('/','-')) {
+        scm {
+           git {
+              remote {
+                 url "${git_scm_url}"
+              }
+              branch it.name
+              createTag false
+              clean true
+           }
+        }
+        steps {
+           maven("clean verify")
         }
     }
 }
