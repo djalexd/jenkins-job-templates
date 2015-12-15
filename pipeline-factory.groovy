c0d8f0e7588d57bb59301ea4a8dc15049d96da41
Alexandru Dobjanschi <djalexd@users.noreply.github.com>
2015-12-15 03:26:41 +0200

    Add start job

--- pipeline-factory.groovy
+++ pipeline-factory.groovy
@@ -3,6 +3,10 @@ def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each {
     def branchName = it.name
+    job("${project} - start - ${branchName}".replaceAll('/', '-')) {
+        /* Do nothing, this is just a placeholder */
+    }
+    
     job("${project} - unit-tests - ${branchName}".replaceAll('/','-')) {
         /* the swarm label is a future extension */
         /*label("swarm")*/
