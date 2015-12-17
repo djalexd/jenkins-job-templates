b0a1e573576b0953f2faa81a9b26d95d9855546d
Alexandru Dobjanschi <djalexd@users.noreply.github.com>
2015-12-17 15:05:16 +0200

    Update pipeline-factory.groovy

--- pipeline-factory.groovy
+++ pipeline-factory.groovy
@@ -1,4 +1,27 @@
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+/**
+ * Extracts the project name, based on github project url:
+ * i.e.
+ *   git@github.com:test1/proj.git => test1/proj
+ *   https://github.com/test1/proj.git => test1/proj
+ */ 
+​def extractGitHubProjectName(String githubProjectUrl) {    
+    def lowerCase = githubProjectUrl.toLowerCase(), 
+        length = lowerCase.length(), 
+        idx1 = lowerCase.indexOf("github.com"),
+        idx2 = idx1 + "github.com".length() + 1,
+        idx3 = lowerCase.lastIndexOf(".git")
+    if (idx1 < 0) {
+        throw new IllegalArgumentException(String.format("Illegal GitHub project url: %s", githubProjectUrl))
+    }
+    if (idx3 < 0) {
+        lowerCase.substring(idx2, lowerCase.length())
+    } else {
+        lowerCase.substring(idx2, idx3)
+    }
+}
+
+def gitHutProjectName = extractGitHubProjectName("${git_scm_url}")
+def branchApi = new URL("https://api.github.com/repos/${gitHutProjectName}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each {
     def branchName = it.name
@@ -7,6 +30,7 @@ branches.each {
         "${project} - start - ${branchName}",
         "${project} - unit-tests - ${branchName}",
         "${project} - integration-tests - ${branchName}",
+        "${project} - Sonar analysis - ${branchName}"
         "${project} - done - ${branchName}"
     ] as String[]
     
