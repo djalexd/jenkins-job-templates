e79ac385f5ac2443dc960cf425a18f0292e0ecc5
Alex Dobjanschi <alex.dobjanschi@gmail.com>
2016-01-01 23:20:42 +0200

    update java pipeline factory

--- pipeline-factory.groovy
+++ pipeline-factory.groovy
@@ -8,14 +8,15 @@ branches.each {
     def branchName = it.name
     /* Configure all job names here for easier referencing */
     List<String> jobNames = [
-        "${project} - start - ${branchName}",
-        "${project} - unit-tests - ${branchName}",
-        "${project} - integration-tests - ${branchName}",
-        "${project} - Sonar analysis - ${branchName}",
-        "${project} - done - ${branchName}"
+        "${project}/${branchName}/start",
+        "${project}/${branchName}/unit-tests",
+        "${project}/${branchName}/integration-tests",
+        "${project}/${branchName}/sonar-analysis",
+        "${project}/${branchName}/nexus-deploy",
+        "${project}/${branchName}/done"
     ] as String[]
     
-    def sanitizedJobNames = jobNames.collect { it.replaceAll('/', '-') }
+    def sanitizedJobNames = jobNames
     
     job(sanitizedJobNames.get(0)) {
         /* Do nothing, this is just a placeholder */
@@ -95,18 +96,18 @@ branches.each {
             }
         }
         
-        publishers {
+        /*publishers {
             sonar {
                 branch(branchName)
                 overrideTriggers {
                     skipIfEnvironmentVariable('SKIP_SONAR')
                 }
             }
-        }
+        }*/
 
         steps {
-            maven("clean install -DskipTests")
-            /*maven("sonar:sonar")*/
+            maven("clean install")
+            maven("sonar:sonar -Dsonar.host.url=http://sonar:9000")
             downstreamParameterized {
                 trigger(sanitizedJobNames.get(4)) {
                     parameters {
@@ -120,4 +121,21 @@ branches.each {
     job(sanitizedJobNames.get(4)) {
         deliveryPipelineConfiguration("done")
     }
+
+    deliveryPipelineView("${project}-${branch}-view") {
+        pipelineInstances(5)
+        showAggregatedPipeline()
+        columns(2)
+        updateInterval(2)
+        enableManualTriggers()
+        showAvatars()
+        showChangeLog()
+        pipelines {
+            /*component('Sub System A', 'compile-a')
+            component('Sub System B', 'compile-b')*/
+            // ${project} - start - ${branchName}
+            regex(/(.*)-start/)
+        }
+    }
+
 }
